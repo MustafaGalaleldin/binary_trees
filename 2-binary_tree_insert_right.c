7d06ea98a4b6ee6a129b7bbae4cc4ae2fbff60c2
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-06-05 22:49:22 +0300

    ->

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,9 +8,12 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = binary_tree_node(parent, value);
+	binary_tree_t *node;
 
-	if (parent == NULL || node == NULL)
+	if (parent == NULL)
+		return (NULL);
+	node = binary_tree_node(parent, value);
+	if (!node)
 		return (NULL);
 	if (parent->right == NULL)
 		parent->right = node;
